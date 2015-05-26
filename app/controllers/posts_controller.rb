15d5cde2bba349c3e4f1e0763fa1c729e1147d66
bdthinh <bdthinh@fit.hcmus.edu.vn>
2015-05-27 00:51:18 +0700

    Feature post done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   # GET /posts
   # GET /posts.json
   def index
